8f0b8e89f9ae909303957c93d275f30f0891df84
jaskirat1208 <jaskiratsingh1208@gmail.com>
2017-12-01 21:39:25 +0530

    Skeleton code for assignment 2

---
+++ assign2/src/com/jaskirat/Candidate.java
@@ -0,0 +1,11 @@
+package com.jaskirat;
+
+public class Candidate {
+	Transaction tx;
+	int sender;
+	
+	public Candidate(Transaction tx, int sender) {
+		this.tx = tx;
+		this.sender = sender;
+	}
+}
